3199ded143d5327e10a99dbe7eb02eb324a4260e
munyite001 <emunyite@gmail.com>
2023-06-19 22:34:09 +0300

    Project Setup and main functionality

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Video File',
+        type: 'video',
+        path: '/file-server/pancake.mp4'
+    },
+    {
+        id: 2,
+        name: 'Audio File',
+        type: 'audio',
+        path: '/file-server/avengers.mp3'
+    },
+    {
+        id: 3,
+        name: 'Document File',
+        type: 'document',
+        path: '/file-server/recipes.pdf'
+    },
+    {
+        id: 4,
+        name: 'Image File',
+        type: 'image',
+        path: '/file-server/wifi.jpg'
+    },
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: '/file-server/elite.jpg'
+    },
+    {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: '/file-server/python.jpeg'
+    }
+]
