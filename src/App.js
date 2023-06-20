e3e0d6ec4120beba6e1e4063f6b80f8995b7f070
munyite001 <emunyite@gmail.com>
2023-06-20 09:04:52 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import { AudioPlayer } from './components/AudioPlayer';
 import { DocumentViewer } from './components/DocumentViewer';
 import { VideoPlayer } from './components/VideoPlayer';
 import { ImageViewer } from './components/ImageViewer';
+import { Pie, Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  ArcElement,
+  Tooltip,
+  Legend
+ } from 'chart.js';
+ ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+ );
 
 export default function App() {
   const [myFiles, setMyFiles] = useState([]);
@@ -15,9 +35,82 @@ export default function App() {
   useEffect(() => {
     setMyFiles(data)
   }, [])
+  var barChartOptions = {
+    responsive: true,
+    plugins: {
+     legend: {
+      position: 'top',
+     },
+     title: {
+      display: true,
+      text: 'Files Breakdown',
+     },
+    },
+   };
 
   return (
     <>
+    {showChartModal && (
+    <div style={styles.modal}>
+     <div style={styles.modalContent}>
+      <div style={styles.modalHeader}>
+       <p style={{ fontWeight: "bold" }}>Files Breakdown</p>
+       <button style={styles.closeButton} onClick={() => setShowChartModal(false)}>close</button>
+      </div>
+      <div style={styles.modalBody}>
+       <Pie
+        data={{
+         labels: ['Video', 'Audio', 'Document', 'Image'],
+         datasets: [
+          {
+           label: 'Files Breakdown',
+           data: [myFiles.filter(file => file.type === 'video').length, myFiles.filter(file => file.type === 'audio').length, myFiles.filter(file => file.type === 'document').length, myFiles.filter(file => file.type === 'image').length],
+           backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+           ],
+           borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+           ],
+           borderWidth: 1,
+          },
+         ],
+        }}
+       />
+       <Bar
+        data={{
+         labels: ['Video', 'Audio', 'Document', 'Image'],
+         datasets: [
+          {
+           label: 'Files Breakdown',
+           data: [myFiles.filter(file => file.type === 'video').length, myFiles.filter(file => file.type === 'audio').length, myFiles.filter(file => file.type === 'document').length, myFiles.filter(file => file.type === 'image').length],
+           backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+           ],
+           borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+           ],
+           borderWidth: 1,
+          },
+         ],
+        }}
+        options={barChartOptions}
+       />
+      </div>
+     </div>
+    </div>
+   )}
       <div className='App'>
         <Header />
         <div style={styles.container}>
@@ -26,6 +119,46 @@ export default function App() {
             <p>{selectedFile ? selectedFile.path : filePath}</p>
           </div>
           <div style={styles.fileContainer}>
+            <div style={styles.controlTools}>
+              <button style={styles.controlButton} 
+                onClick={() => {
+                  if (selectedFile) {
+                    const newFiles = myFiles.map(file => {
+                      if (file.id === selectedFile.id) {
+                        return {
+                          ...file,
+                          name: prompt("Enter new name")
+                        }
+                      }
+                      return file
+                    })
+                    setMyFiles(newFiles)
+                    setSelectedFile(null)
+                  }
+                }}>Rename</button>
+                <button style={styles.controlButton}
+                onClick={() => {
+                  setShowChartModal(true)
+                }}>Files Breakdown</button>
+                <button style={styles.controlButton}
+                onClick={() => {
+                  if (selectedFile){
+                    window.open(selectedFile.path, "_blank")
+                  }
+                }}>Download</button>
+                <button style={styles.controlButton}
+                onClick={() => {
+                  if (selectedFile) {
+                    const newFiles = myFiles.filter(file => {
+                      if (file.id !== selectedFile.id){
+                        return file
+                      }
+                    })
+                    setMyFiles(newFiles)
+                    setSelectedFile(null)
+                  }
+                }}>Delete</button>
+            </div>
             <div style={{ width: "100%", padding: 10 }}>
               {myFiles.map((file) => {
 
@@ -110,4 +243,55 @@ const styles = {
    cursor: 'pointer',
    fontWeight: 'bold',
   },
+  // modal
+  modal: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: '20px',
+    height: '50vh',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'column',
+  },
+  modalClose: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    padding: '10px',
+    cursor: 'pointer',
+  },
+  modalBody:{
+    width: '100%',
+    height: '90%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    padding: '10px',
+  },
+  modalHeader: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  closeButton: {
+    padding: '10px',
+    border: 'none',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    backgroundColor: '#eee',
+  }
  };
